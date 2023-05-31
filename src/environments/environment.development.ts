70b8b0ca5eef8ad7487ee76c991fd1b700b1982f
Rouslan Khayaouri <rouslan_kh@hotmail.com>
2023-05-31 22:59:36 +0200

    env file modifications

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,7 @@
 export const environment = {
   version: '0.0.3-DEV',
   submit_server_url: 'http://localhost',
-  submit_server_port: 9003
+  submit_server_port: 9003,
+  read_server_url: 'http://localhost',
+  read_server_port: 9004
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   version: '0.0.3-PROD',
-  submit_server_url: 'http://taco-order.service',
-  submit_server_port: 9003
+  submit_server_url: 'http://submit-taco-order-service',
+  submit_server_port: 9003,
+  read_server_url: 'http://taco-order-service',
+  read_server_port: 9004
 };
