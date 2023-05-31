b03d6c2022b9994153d4de45b3ba19e1863f657a
Rouslan Khayaouri <rouslan_kh@hotmail.com>
2023-05-31 22:36:43 +0200

    link to order list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,12 @@ import {OrderListComponent} from "./components/order-list/order-list.component";
 import {OrderSubmittedComponent} from "./components/order-submitted/order-submitted.component";
 import {WelcomeComponent} from "./components/welcome/welcome.component";
 import {CreateOrderComponent} from "./components/create-order/create-order.component";
-import {OrderSummary} from "./model/Order";
 
 const routes: Routes = [
   {path: '', component: WelcomeComponent, pathMatch: "full"},
   {path: 'orders', component: OrderListComponent},
   {path: 'create-order', component: CreateOrderComponent},
-  {path: 'order-submitted', component: OrderSubmittedComponent, data: {test: "test"}},
+  {path: 'order-submitted', component: OrderSubmittedComponent},
   {path: '**', redirectTo: ''}
 ];
 
